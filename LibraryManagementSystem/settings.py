6082f14b9c68cb756410e82e11ea7f3bee37ef77
Sami Ullah Saleem <samiullah1701734@gmail.com>
2023-04-01 10:29:59 +0000

    .gitignore add

--- LibraryManagementSystem/settings.py
+++ LibraryManagementSystem/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "LibraryManagementSystem.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'samidatabase',
+        'USER': 'samiullahsaleem',
+        'PASSWORD': 'samiullahsaleem',
+        'HOST': 'db4free.net',
+        'PORT': '3306',
     }
 }
 
