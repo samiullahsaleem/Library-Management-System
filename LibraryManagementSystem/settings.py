7ab73c5a4211f0a8d9e10b6c29118bd1c6d3c92b
Sami Ullah Saleem <samiullah1701734@gmail.com>
2023-04-10 19:43:52 +0000

    c2

--- LibraryManagementSystem/settings.py
+++ LibraryManagementSystem/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-!zakb2wer1#+olyq%)y4s2mm^x4+$-$imbccu_%ti3nn_onkd*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = FALSE
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -126,7 +126,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = "/"
+
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 
 # Default primary key field type
@@ -134,3 +141,5 @@ STATIC_ROOT = "/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Update database configuration from $DATABASE_URL.
+import django_heroku
+django_heroku.settings(locals())
