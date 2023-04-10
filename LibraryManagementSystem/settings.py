fc1955ed9772338df029656c108237d22608099f
Sami Ullah Saleem <samiullah1701734@gmail.com>
2023-04-10 19:10:29 +0000

    m2

--- LibraryManagementSystem/settings.py
+++ LibraryManagementSystem/settings.py
@@ -133,6 +133,3 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
